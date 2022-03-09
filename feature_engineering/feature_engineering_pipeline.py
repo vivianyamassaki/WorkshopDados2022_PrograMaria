df4d60070c79cdbbc594ee9b962476ef9d6989c4
vivianyamassaki <vivian.yamassaki@creditas.com.br>
2022-03-09 00:53:17 -0300

    volta método de get pipeline

--- feature_engineering/feature_engineering_pipeline.py
+++ feature_engineering/feature_engineering_pipeline.py
@@ -1,10 +1,6 @@
-"""
-Script que contém o pipeline da feature engineering. Ele chama scripts secundários que fazem as transformações necessárias
-dos dados e na ordem em que constam no pipeline
-"""
-
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
+
 from feature_engineering.missing_imputer import MissingValuesImputer
 from feature_engineering.numerical_scaler import NumericalFeaturesScaler
 from feature_engineering.ohe_transformer import OneHotEncode
@@ -14,13 +10,12 @@ class FeatureEngineering(BaseEstimator, TransformerMixin):
 
     def __init__(self, numerical_features):
         self.numerical_features = numerical_features
-        self.pipeline = Pipeline(
+
+    def get_pipeline(self):
+        return Pipeline(
             [
                 ("ohe", OneHotEncode()),
                 ("missing_imputer", MissingValuesImputer()),
                 ("numerical_scaler", NumericalFeaturesScaler(numerical_features=self.numerical_features)),
             ]
         )
-
-    def transform(self, data):
-        return self.pipeline.transform(data)
