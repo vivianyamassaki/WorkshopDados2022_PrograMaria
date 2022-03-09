ed0b11c088845ef28cd2923795dbd5b69dffbad9
vivianyamassaki <vivian.yamassaki@creditas.com.br>
2022-03-09 00:55:56 -0300

    atualiza utilização do pipeline

--- predictor/predict.py
+++ predictor/predict.py
@@ -3,16 +3,17 @@ import settings as st
 
 from feature_engineering.feature_engineering_pipeline import FeatureEngineering
 
-class Predict():
+
+class Predict:
 
     def __init__(self):
         self.model_path = st.model_path
         self.model = load_pickle(self.model_path)
 
     def predict(self, data):
-        processed_data = FeatureEngineering(st.numerical_features).transform(data)
-        result= self.model.predict_proba(processed_data)[0]
+        processed_data = FeatureEngineering(st.numerical_features).get_pipeline().transform(data)
+        result = self.model.predict_proba(processed_data)[0]
         status = 'morreu'
-        if result[0] > 0.5:
+        if result[1] > 0.5:
             status = 'sobreviveu'
-        return status, result[0]
+        return status, result[1]
